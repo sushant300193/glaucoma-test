618d7e9522fab0bdb9f2f18dcaff3e92ab831d04
sushantsgmoid <sushantsgmoid@gmail.com>
2019-12-31 13:32:23 +0530

    uploading a glau demo project with google drive download link

--- google-app-engine/app/server.py
+++ google-app-engine/app/server.py
@@ -8,7 +8,7 @@ from io import BytesIO
 from fastai import *
 from fastai.vision import *
 
-model_file_url = 'https://1drv.ms/u/s!ArSxnpJlOnFWiGKdWPxHZG7YKJqF?e=3Ege8K'
+model_file_url = 'https://drive.google.com/uc?id=17bNCRls1x13Ov5QJ6NCt5WqGFLB7TllP&export=download'
 model_file_name = 'model'
 classes = ['GLAUCOMA', 'NORMAL', 'SUSPECT']
 path = Path(__file__).parent
